567f74a0c8c08065e6f4f75129377a714cb9bdce
Edimaique <edimaiqueacacio@gmail.com>
2025-03-25 22:05:55 -0300

    tirando a / do final do endereço da api

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from "react";
 import { BsTrash, BsBookmarkCheck, BsBookmarkCheckFill } from "react-icons/bs";
 import "./App.css";
 
-const API = "https://server-listdo.onrender.com/";
+const API = "https://server-listdo.onrender.com";
 
 function App() {
   const [title, setTitle] = useState("");
